c10c5d2cf5e972cdc82917dc3fb20d68429aa268
bert <btemme@hotmail.com>
2018-12-18 14:50:44 +0100

    adding benchmark test project

---
+++ b3dm.tile.benchmarks/ParsingBenchmark.cs
@@ -0,0 +1,25 @@
+﻿using B3dm.Tile;
+using BenchmarkDotNet.Attributes;
+using System.IO;
+
+namespace b3dm.tile.benchmarks
+{
+    public class ParsingBenchmark
+    {
+        private readonly Stream stream;
+
+        public ParsingBenchmark()
+        {
+            string infile = "testfixtures/1311.b3dm";
+            stream = new MemoryStream(File.ReadAllBytes(infile));
+        }
+
+        [Benchmark]
+        public byte[] ParseB3dmTileFromStream()
+        {
+            var glb = B3dmParser.ParseB3dm(stream).GlbData;
+            return glb;
+        }
+
+    }
+}
---
+++ b3dm.tile.benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace b3dm.tile.benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ParsingBenchmark>();
+        }
+    }
+}
