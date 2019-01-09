7679996c687f689c9dfb44aa02121fb8b113c798
bert <btemme@hotmail.com>
2019-01-09 15:19:13 +0100

    clean up unit test for issue 3

--- b3dm.tile.tests/TestIssue3.cs
+++ b3dm.tile.tests/TestIssue3.cs
@@ -10,14 +10,18 @@ namespace B3dm.Tile.Tests
         {
             // issue https://github.com/bertt/b3dm-tile-cs/issues/3
             // arrange
+            // texel tiles does not have correct gltf version
             const string texeltestfile = "B3dm.Tile.Tests.testfixtures.2.b3dm";
             var streamtexel = Assembly.GetExecutingAssembly().GetManifestResourceStream(texeltestfile);
             var b3dmtextel = B3dmParser.ParseB3dm(streamtexel);
-            // textel b3dm has another format of gltf (1?)
+            // eurgh what is 576404002? it's not 2...
+            Assert.IsTrue(GltfVersionChecker.GetGlbVersion(b3dmtextel.GlbData) == 576404002);
 
+            // amsterdam tile does have correct gltf version (2)
             const string amstestfile = "B3dm.Tile.Tests.testfixtures.1311.b3dm";
             var streamams= Assembly.GetExecutingAssembly().GetManifestResourceStream(amstestfile);
             var b3dmams = B3dmParser.ParseB3dm(streamams);
+            Assert.IsTrue(GltfVersionChecker.GetGlbVersion(b3dmams.GlbData) == 2);
         }
     }
 }
