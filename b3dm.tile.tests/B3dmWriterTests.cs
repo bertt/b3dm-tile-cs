c0431562452ad42c5b4f041e471517ae47dccac9
bert <btemme@hotmail.com>
2019-02-20 20:50:50 +0100

    add reading wkb in tests

---
+++ b3dm.tile.tests/B3dmWriterTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+using Wkx;
+
+namespace B3dm.Tile.Tests
+{
+    public class B3dmWriterTests
+    {
+        [Test]
+        public void WriteB3dmTest()
+        {
+            const string testfile = "B3dm.Tile.Tests.testfixtures.building.wkb";
+            var buildingWkb = Assembly.GetExecutingAssembly().GetManifestResourceStream(testfile);
+            var g = Geometry.Deserialize<WkbSerializer>(buildingWkb);
+            Assert.IsTrue(g.GeometryType == GeometryType.PolyhedralSurface);
+            Assert.IsTrue(((PolyhedralSurface)g).Geometries.Count==15);
+        }
+    }
+}
