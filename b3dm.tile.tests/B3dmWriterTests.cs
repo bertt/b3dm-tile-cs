baabda3f1b139853021a967870af65e2559bcbd3
bert <btemme@hotmail.com>
2019-02-21 13:40:11 +0100

    starting conversion wkb -> gltf

--- b3dm.tile.tests/B3dmWriterTests.cs
+++ b3dm.tile.tests/B3dmWriterTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using System.Reflection;
 using Wkx;
 
@@ -13,7 +14,32 @@ namespace B3dm.Tile.Tests
             var buildingWkb = Assembly.GetExecutingAssembly().GetManifestResourceStream(testfile);
             var g = Geometry.Deserialize<WkbSerializer>(buildingWkb);
             Assert.IsTrue(g.GeometryType == GeometryType.PolyhedralSurface);
-            Assert.IsTrue(((PolyhedralSurface)g).Geometries.Count==15);
+            var poly = ((PolyhedralSurface)g);
+            Assert.IsTrue(poly.Geometries.Count==15);  // 15 polygons
+            Assert.IsTrue(poly.Geometries[0].ExteriorRing.Points.Count == 5);  // 5 points in exteriorring of first geometry
+
+            var bb = poly.GetBoundingBox();
+            Assert.IsTrue(Math.Round(bb.XMin, 2) == -8.75);
+            Assert.IsTrue(Math.Round(bb.YMin, 2) == -7.36);
+            Assert.IsTrue(Math.Round(bb.XMax, 2) == 8.80);
+            Assert.IsTrue(Math.Round(bb.YMax, 2) == 7.30);
+            // q: do we need the min-z, max-z?
+
+            //  ts = TriangleSoup.from_wkb_multipolygon(wkb)
+
+            //#define the geometry's world transformation
+            //    >>> transform = np.array([
+            //    ...             [1, 0, 0, 1842015.125],
+            //    ...             [0, 1, 0, 5177109.25],
+            //    ...             [0, 0, 1, 247.87364196777344],
+            //    ...             [0, 0, 0, 1]], dtype=float)
+
+            // todo: convert to gltf
+            // need: positions, normals, uv's, bbox
+
+            // todo: convert to glb
+
+            // todo: convert to b3dm
         }
     }
 }
