713522587c8bf2a27a208bc475d23d62e6575cd5
bert <btemme@hotmail.com>
2019-02-28 15:15:15 +0100

    add polyhedralsurfacec boundingbox3d method

--- b3dm.tile.tests/B3dmWriterTests.cs
+++ b3dm.tile.tests/B3dmWriterTests.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Reflection;
 using Wkx;
 
@@ -13,16 +12,8 @@ namespace B3dm.Tile.Tests
             const string testfile = "B3dm.Tile.Tests.testfixtures.building.wkb";
             var buildingWkb = Assembly.GetExecutingAssembly().GetManifestResourceStream(testfile);
             var g = Geometry.Deserialize<WkbSerializer>(buildingWkb);
-            Assert.IsTrue(g.GeometryType == GeometryType.PolyhedralSurface);
             var polyhedralsurface = ((PolyhedralSurface)g);
-            Assert.IsTrue(polyhedralsurface.Geometries.Count == 15);  // 15 polygons
-            Assert.IsTrue(polyhedralsurface.Geometries[0].ExteriorRing.Points.Count == 5);  // 5 points in exteriorring of first geometry
-
             var bb = polyhedralsurface.GetBoundingBox();
-            Assert.IsTrue(Math.Round(bb.XMin, 2) == -8.75);
-            Assert.IsTrue(Math.Round(bb.YMin, 2) == -7.36);
-            Assert.IsTrue(Math.Round(bb.XMax, 2) == 8.80);
-            Assert.IsTrue(Math.Round(bb.YMax, 2) == 7.30);
 
             var triangles = Triangulator.Triangulate(polyhedralsurface);
             Assert.IsTrue(triangles.Count == 22);
@@ -33,7 +24,11 @@ namespace B3dm.Tile.Tests
             var bytesNormals = triangles.NormalsToBinary();
             Assert.IsTrue(bytesNormals.Length == 792);
 
-            // todo: get data array
+            // todo: make gltf
+            // in pyhton    glTF = GlTF.from_binary_arrays(arrays, transform)
+
+            // todo: make b3dm
+            // in python: B3dm.from_glTF(glTF)
         }
     }
 }
---
+++ b3dm.tile.tests/BoundingBox3D.cs
@@ -0,0 +1,14 @@
+﻿using Wkx;
+
+namespace B3dm.Tile.Tests
+{
+    public class BoundingBox3D
+    {
+        public double XMin { get; set; }
+        public double XMax { get; set; }
+        public double YMin { get; set; }
+        public double YMax { get; set; }
+        public double ZMin { get; set; }
+        public double ZMax { get; set; }
+    }
+}
