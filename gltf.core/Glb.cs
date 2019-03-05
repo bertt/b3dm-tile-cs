d88bdff8e7bfebe26c2c036502dddf5da7f2db81
bert <btemme@hotmail.com>
2019-03-05 09:53:09 +0100

    adding GlbReader again

---
+++ gltf.core/Glb.cs
@@ -0,0 +1,11 @@
+﻿namespace Gltf.Core
+{
+    public class Glb
+    {
+        public uint Magic { get; set; }
+        public uint Version { get; set; }
+        public string GltfModelJson { get; set; }
+        public byte[] GltfModelBin { get; set; }
+        public uint Length { get; set; }
+    }
+}
