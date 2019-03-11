1a27b73d7daf5b6f318ebbf3a04d2d9a314ac4ee
bert <btemme@hotmail.com>
2019-03-11 16:20:33 +0100

    add pg2b3dm

--- src/samples/pg2b3dm/Program.cs
+++ src/samples/pg2b3dm/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using B3dm.Tile;
+using Gltf.Core;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
 
@@ -11,13 +13,16 @@ namespace pg2b3dm
         {
             Console.WriteLine("tool: pg2b3dm");
             Console.WriteLine("version: alpha alpha alpha");
-
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
             var configuration = builder.Build();
             var connectionString = configuration["connectionstring"];
-            var table = configuration["geometry_table"];
+            var geometry_table = configuration["geometry_table"];
+            var geometry_column = configuration["geometry_column"];
+
             var conn = new NpgsqlConnection(connectionString);
             conn.Open();
+
+            var bbox3d = GetBoundingBox3D(conn, geometry_table, geometry_column);
 
             var cmd = new NpgsqlCommand("SELECT * from tmp.tmp", conn);
             var reader = cmd.ExecuteReader();
@@ -27,6 +32,23 @@ namespace pg2b3dm
             conn.Close();
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+        }
+
+
+        private static BoundingBox3D GetBoundingBox3D(NpgsqlConnection connection, string geometry_table, string geometry_column)
+        {
+            // read bounding box
+            var cmd = new NpgsqlCommand($"SELECT st_xmin(geom1), st_ymin(geom1), st_zmin(geom1), st_xmax(geom1), st_ymax(geom1), st_xmax(geom1) FROM (select ST_3DExtent({geometry_column}) as geom1 from {geometry_table}) as t", connection);
+            var reader = cmd.ExecuteReader();
+            reader.Read();
+            var xmin = reader.GetDouble(0);
+            var ymin = reader.GetDouble(1);
+            var zmin = reader.GetDouble(2);
+            var xmax = reader.GetDouble(3);
+            var ymax = reader.GetDouble(4);
+            var zmax = reader.GetDouble(5);
+            reader.Close();
+            return new BoundingBox3D() { XMin = xmin, YMin = ymin, ZMin = zmin, XMax = xmax, YMax = ymax, ZMax = zmax };
         }
     }
 }
