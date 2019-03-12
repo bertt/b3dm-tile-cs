4cc6f57d882cc861192eb492d0a4cb18e4897c21
bert <btemme@hotmail.com>
2019-03-12 12:18:57 +0100

    testing using gltf2loader library

---
+++ src/b3dm.tile.tests/GltfLoaderTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using glTFLoader;
+using NUnit.Framework;
+
+namespace B3dm.Tile.Tests
+{
+    public class GltfLoaderTests
+    {
+        [Test]
+        public void LoadGltf()
+        {
+            var gltf = Interface.LoadModel(@"testfixtures/2CylinderEngine.gltf");
+            var stream = File.OpenRead(@"testfixtures/2CylinderEngine0.bin");
+            var reader = new BinaryReader(stream);
+            var bytes = reader.ReadBytes((int)reader.BaseStream.Length);
+            foreach (var buf in gltf.Buffers) {
+                buf.Uri = null;
+            }
+            gltf.SaveBinaryModel(bytes, "test.glb");
+        }
+    }
+}
