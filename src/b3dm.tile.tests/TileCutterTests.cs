d6b98b424404ad850d447068049996c687b1aa78
bert <btemme@hotmail.com>
2019-04-11 17:01:55 +0200

    wip tileset.json

--- src/b3dm.tile.tests/TileCutterTests.cs
+++ src/b3dm.tile.tests/TileCutterTests.cs
@@ -12,29 +12,38 @@ namespace B3dm.Tile.Tests
         public void TileCutterFirstTests()
         {
             // arrange
-            var zUpBoxes = GetTestData();
+            var zUpBoxes = GetTestData("testfixtures/bboxes.txt");
+            var bboxes_gropuped_expected = GetTestData("testfixtures/bboxes_grouped_expected.txt");
 
             // act
-            var tree= TileCutter.ConstructTree(zUpBoxes);
+            var tree = TileCutter.ConstructTree(zUpBoxes);
+            var intfeatures = GetNrOfChildren(tree);
+            Assert.IsTrue(intfeatures == bboxes_gropuped_expected.Count);
             var bbox0 = tree.Children[0].GetBoundingBox3D();
+
             // assert
             Assert.IsTrue(zUpBoxes.Count == 1580);
             Assert.IsTrue(tree.Children[0].Features.Count == 20);
             Assert.IsTrue(tree.Children[0].Features[0].Id == 0);
             Assert.IsTrue(tree.Children[0].Children[0].Features[0].Id == 20);
-            Assert.IsTrue(bbox0.XMin == -183.897999999986);
-            Assert.IsTrue(bbox0.XMax == 183.847000000009);
-            Assert.IsTrue(bbox0.YMin == -134.141648505291);
-            Assert.IsTrue(bbox0.YMax == 138.625);
-            Assert.IsTrue(bbox0.ZMin == -11.7305252024974);
-            Assert.IsTrue(bbox0.ZMax == 11.7305220562124);
+            Assert.IsTrue(Math.Round(bbox0.XMin,1) == Math.Round(bboxes_gropuped_expected[0].XMin,1));
+            Assert.IsTrue(Math.Round(bbox0.YMin, 0) == Math.Round(bboxes_gropuped_expected[0].YMin, 0));
+            Assert.IsTrue(Math.Round(bbox0.ZMin, 3) == Math.Round(bboxes_gropuped_expected[0].ZMin, 3));
             var c = bbox0.GetCenter();
+        }
+        public int GetNrOfChildren(Node tree)
+        {
+            var f = tree.Children.Count;
+            foreach(var c in tree.Children) {
+                f+= GetNrOfChildren(c);
+            }
+            return f;
         }
 
 
-        private static List<BoundingBox3D> GetTestData()
+        private static List<BoundingBox3D> GetTestData(string file)
         {
-            var fileStream = new FileStream("testfixtures/bboxes.txt", FileMode.Open);
+            var fileStream = new FileStream(file, FileMode.Open);
             var bboxes = new List<BoundingBox3D>();
             var reader = new StreamReader(fileStream);
             string line;
