e88b2c5175a71224cb13e462e49f425f9bcf0311
bert <btemme@hotmail.com>
2019-05-09 13:52:00 +0200

    prepare for 0.5 release b3dm.tile

---
+++ src/b3dm.tile.tests/B3dmWriterTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace B3dm.Tile.Tests
+{
+    public class B3dmWriterTest
+    {
+        [Test]
+        public void WriteB3dmTest()
+        {
+            // todo: convert glb to b3dm....
+
+            // arrange
+
+            // act
+
+            // assert
+        }
+    }
+}
