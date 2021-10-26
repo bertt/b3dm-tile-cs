3a20f922f4ad80ce143d4186bedb8f8cff6bdedb
bertt <btemme@hotmail.com>
2021-10-26 12:22:41 +0200

    fix cyrrlic characters

--- src/b3dm.tile.tests/B3dmWriterTest.cs
+++ src/b3dm.tile.tests/B3dmWriterTest.cs
@@ -51,5 +51,41 @@ namespace B3dm.Tile.Tests
             Assert.IsTrue(b3dmActual.B3dmHeader.Version== b3dmExpected.B3dmHeader.Version);
             Assert.IsTrue(b3dmActual.B3dmHeader.FeatureTableJsonByteLength== b3dmExpected.B3dmHeader.FeatureTableJsonByteLength);
         }
+
+
+        [Test]
+        public void WriteB3dmWithBatchCyrrilicTest()
+        {
+            // arrange
+            var buildingGlb = File.ReadAllBytes(@"testfixtures/with_batch.glb");
+            var batchTableJson = File.ReadAllText(@"testfixtures/BatchTableWithCyrillicCharacters.json");
+
+            var b3dmBytesExpected = File.OpenRead(@"testfixtures/with_batch.b3dm");
+            var b3dmExpected = B3dmReader.ReadB3dm(b3dmBytesExpected);
+            var errors = b3dmExpected.B3dmHeader.Validate();
+            Assert.IsTrue(errors.Count > 0);
+
+            var b3dm = new B3dm(buildingGlb);
+            b3dm.BatchTableJson = batchTableJson;
+            b3dm.FeatureTableJson = b3dmExpected.FeatureTableJson;
+            b3dm.BatchTableJson = batchTableJson;
+            b3dm.FeatureTableBinary = b3dmExpected.FeatureTableBinary;
+            b3dm.BatchTableBinary = b3dmExpected.BatchTableBinary;
+
+            // act
+            var result = "with_batch_cyrrilic.b3dm";
+            var bytes = b3dm.ToBytes();
+            File.WriteAllBytes(result, bytes);
+            var b3dmActual = B3dmReader.ReadB3dm(File.OpenRead(result));
+
+            // Assert
+            var errorsActual = b3dmActual.B3dmHeader.Validate();
+            Assert.IsTrue(errorsActual.Count == 0);
+
+            Assert.IsTrue(b3dmActual.B3dmHeader.Magic == b3dmExpected.B3dmHeader.Magic);
+            Assert.IsTrue(b3dmActual.B3dmHeader.Version == b3dmExpected.B3dmHeader.Version);
+            Assert.IsTrue(b3dmActual.B3dmHeader.FeatureTableJsonByteLength == b3dmExpected.B3dmHeader.FeatureTableJsonByteLength);
+        }
+
     }
 }
